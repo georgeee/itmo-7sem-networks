9ae095a48958748078ef2c12fd42a8995a968ad3
Alexander Rakovsky <str1t3r@gmail.com>
2015-12-19 14:13:23 +0300

    start-stop
    added full start-stop mechanics

--- lab3/src/main/java/ru/ifmo/ctd/year2012/sem7/networks/lab3/Application.java
+++ lab3/src/main/java/ru/ifmo/ctd/year2012/sem7/networks/lab3/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import ru.ifmo.ctd.year2012.sem7.networks.lab3.transmission.Receiver;
 import ru.ifmo.ctd.year2012.sem7.networks.lab3.transmission.Sender;
 
-import javax.xml.bind.SchemaOutputResolver;
 import java.io.*;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -61,30 +60,44 @@ public class Application implements CommandLineRunner {
             receiver.start();
             audioPlayer.start();
         }
+
         if (settings.isStartSender()) {
-            new Thread(() -> {
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-                    br.lines().forEach(line -> {
-                        Thread recorderThread = new Thread(audioRecorder);
-                        if ("start".equals(line)) {
+            audioRecorder.start();
+            sender.start();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if ("start".equals(line)) {
+                        if (audioRecorder.isRunning()) {
+                            System.err.println("Already running");
+                        } else {
                             if (System.currentTimeMillis() - receiver.getLastReceived() > settings.getLastSendTimeout()) {
-                                sender.start();
-                                recorderThread.start();
+                                audioRecorder.setRunning(true);
                             } else {
                                 log.info("Somebody is holding the line, please try again a bit later");
                             }
-                        } else {
-                            if ("stop".equals(line)) {
-                                recorderThread.interrupt();
-                                sender.interrupt();
-                            }
                         }
-                    });
-                } catch (Exception e) {
-                    log.error("Error in sender example", e);
+                    } else if ("stop".equals(line)) {
+                        if (!audioRecorder.isRunning()) {
+                            System.err.println("Not running");
+                        } else {
+                            audioRecorder.setRunning(false);
+
+                        }
+                    } else if ("exit".equals(line)) {
+                        audioRecorder.setRunning(false);
+                        audioRecorder.interrupt();
+                        sender.interrupt();
+                        receiver.interrupt();
+                        audioPlayer.interrupt();
+                        System.exit(0);
+                        break;
+                    }
                 }
-            }).start();
+            } catch (Exception e) {
+                log.error("Error in sender example", e);
+            }
         }
     }
-    
+
 }
